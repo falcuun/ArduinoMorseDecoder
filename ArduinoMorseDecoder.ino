6e4671e3a4def0ddc101a6a6ca017302cf4cd077
falcuun <falcuun@gmail.com>
2019-02-20 21:19:32 +0100

    Initial Resistor Reading

--- ArduinoMorseDecoder.ino
+++ ArduinoMorseDecoder.ino
@@ -1,2 +1,11 @@
-void setup(){}
-void loop(){}
+ #define LIGHT_SENSOR A0
+
+void setup(){
+    pinMode(LIGHT_SENSOR, INPUT);
+    Serial.begin(9600);
+}
+void loop(){
+    long light_intensity = analogRead(LIGHT_SENSOR);
+    Serial.println(light_intensity);
+    delay(10);
+}
