b3f1741e2cd7674d97c66e0d51fa503c14f94f64
falcuun <falcuun@gmail.com>
2019-02-20 22:42:32 +0100

    Fundaments for signal parsing added

--- ArduinoMorseDecoder.ino
+++ ArduinoMorseDecoder.ino
@@ -1,11 +1,44 @@
- #define LIGHT_SENSOR A0
+#define LIGHT_SENSOR A0
 
-void setup(){
+const char long_signal = '-';
+const char short_signal = '.';
+
+const long long_signal_interval = 30;
+const long letter_space_interval = 70;
+
+void setup()
+{
     pinMode(LIGHT_SENSOR, INPUT);
     Serial.begin(9600);
 }
-void loop(){
-    long light_intensity = analogRead(LIGHT_SENSOR);
-    Serial.println(light_intensity);
+
+unsigned long signal_interval = 0;
+char code;
+
+void loop()
+{
+    int light_intensity = analogRead(LIGHT_SENSOR);
+
+    if (light_intensity > 100)
+    {
+        while (light_intensity >= 100)
+        {
+            signal_interval++;
+            light_intensity = analogRead(LIGHT_SENSOR);
+            delay(10);
+        }
+
+        if (signal_interval >= long_signal_interval)
+        {
+            code = long_signal;
+        }
+        else
+        {
+            code = short_signal;
+        }
+
+        Serial.println(code);
+        pause = 0;
+    }
     delay(10);
 }
